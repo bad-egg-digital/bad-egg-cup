e7111e607cfa254db541e5f7b192b5fe6bed1a85
Steve Ross <git.commits@mailbox.org>
2026-04-02 21:09:31 +0100

    remove post rewrite functions -- mu-plugin is too early to use it

--- app/Features/DefaultPostType.php
+++ app/Features/DefaultPostType.php
@@ -24,15 +24,6 @@ class DefaultPostType
             add_action( 'init', [ $this, 'unregister_tag' ]);
             add_filter( 'register_post_tag_taxonomy_args', [ $this, 'disable' ], 0, 2);
         }
-
-        /*
-         * Default Post Type Customisations
-         */
-        if(current_theme_supports('badeggcup-postRewrite') && get_option('page_for_posts')) {
-            // add_filter( 'registered_post_type', [$this, 'rewrite'], 20, 2 );
-            // add_filter( 'pre_post_link', [$this, 'permalink'], 10, 3);
-            add_filter( 'post_type_labels_post', [$this, 'labels']);
-        }
     }
 
     function disable($args, $type)
@@ -60,48 +51,5 @@ class DefaultPostType
     function unregister_tag()
     {
         unregister_taxonomy_for_object_type('post_tag', 'post');
-    }
-
-    public function labels($labels)
-    {
-        $postsPageID = get_option('page_for_posts');
-        $postsPage = ($postsPageID) ? get_post($postsPageID) : null;
-
-        if($postsPage) {
-            $labels->singular_name = $postsPage->post_title . ' ' . $labels->singular_name;
-            $labels->name = $postsPage->post_title . ' ' . $labels->name;
-            $labels->menu_name = $postsPage->post_title;
-        }
-
-        return $labels;
-    }
-
-    public function rewrite($postType, $args)
-    {
-        if($postType !== 'post') return;
-
-        $postsPageID = get_option('page_for_posts');
-        $postsPage = ($postsPageID) ? get_post($postsPageID) : null;
-
-        if($postsPage) {
-            $args->rewrite = [
-                'slug' => $postsPage->post_name,
-                'with_front' => true,
-            ];
-        }
-
-        return $args;
-    }
-
-    public function permalink($permalink, $post, $leavename)
-    {
-        $postsPageID = get_option('page_for_posts');
-        $postsPage = ($postsPageID) ? get_post($postsPageID) : null;
-
-        if (get_post_type($post) == 'post' && $postsPage) {
-            return $postsPage->post_name . $permalink;
-        } else {
-            return $permalink;
-        }
     }
 }
